f2b3a38e77f4d99160b74d2f420464d1a4c9671d
Kryazhov Roman <kryazhov.roman@mail.ru>
2023-09-02 15:35:06 +0600

    no tests

--- functions.py
+++
@@ -1,7 +0,0 @@
-import json
-
-def load_operations(filename):
-    '''загрузка данных из файла json'''
-    with open(filename, 'rt', encoding='utf-8') as file:
-        data = json.load(file)
-    return data
--- main.py
+++
@@ -1,7 +0,0 @@
-from functions import load_operations
-
-operation_json = 'operations.json'
-
-operations = load_operations(operation_json)
-
-print(operations)
